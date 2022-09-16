9c328c12bdf9bdc7feba8b46bc34f7cace294582
irudayanath <charles.jcs@gmail.com>
2022-09-16 22:59:06 +0530

    Update pyt.py

--- pyt.py
+++ pyt.py
@@ -2,3 +2,8 @@ import requests
 url="https://api.zalando.com/articles"
 response_code=requests.get(url+"/articles")
 print(response_code)
+r = requests.get('https://api.github.com/events')
+r=requests.post('https://httpbin.org/post', data={'key': 'value'})
+r = requests.delete('https://httpbin.org/delete')
+r = requests.head('https://httpbin.org/get')
+r = requests.options('https://httpbin.org/get')
